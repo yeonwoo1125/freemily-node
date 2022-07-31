5f447746e91599b8feca275477c7c93e2776ea42
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-07-31 19:11:21 +0900

    env 환경변수 설정 npm i dotenv -s

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const Chore = require('./chore');
 
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config= require(__dirname + '/../config/config.js')[env]
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
