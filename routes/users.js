34ca5cbc0f1a7656150caf7b89b72629478b38f3
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-09 00:10:30 +0900

    #43 유저 삭제 기능 구현

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const Users = require('../models/user');
+const User = require('../models/user');
 const {validationResult, check} = require("express-validator");
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
@@ -12,46 +12,46 @@ router.post('/',
         check('userName', 'Name is too short').trim().isLength({min: 4})
     ],
     async (req, res, next) => {
-    /*
-      #swagger.tags = ['User']
-      #swagger.description = '유저 생성 POST 요청'
-      #swagger.parameters['obj'] = {
-                in: 'body',
-                description: '유저 생성 정보',
+        /*
+          #swagger.tags = ['User']
+          #swagger.description = '유저 생성 POST 요청'
+          #swagger.parameters['obj'] = {
+                    in: 'body',
+                    description: '유저 생성 정보',
+                    schema: {
+                        userName: 'Hello',
+                        userNickname : 'Hi',
+                        userEmail: 'hehe@naver.com',
+                        userPassword : 'mypassword11'
+                    }
+          }
+          #swagger.responses[201] = {
+                description: '그룹 생성',
                 schema: {
-                    userName: 'Hello',
+                    userId : 1,
+                    userName : 'Hello',
                     userNickname : 'Hi',
-                    userEmail: 'hehe@naver.com',
-                    userPassword : 'mypassword11'
+                    userEmail : 'hehe@naver.com'
                 }
-      }
-      #swagger.responses[201] = {
-            description: '그룹 생성',
-            schema: {
-                userId : 1,
-                userName : 'Hello',
-                userNickname : 'Hi',
-                userEmail : 'hehe@naver.com'
-            }
-      }
-      #swagger.responses[400] = {
-            description : '잘못된 request 형식',
-            schema : {
-                message : [
-                    'Email format is not valid',
-                    'Email is empty',
-                    'Password is too short',
-                    'Name is too short'
-                ]
-            }
-      }
-        #swagger.responses[409] = {
-            description : '이미 가입한 이메일',
-            schema : {
-                message : 'Email is already use'
-            }
-      }
-     */
+          }
+          #swagger.responses[400] = {
+                description : '잘못된 request 형식',
+                schema : {
+                    message : [
+                        'Email format is not valid',
+                        'Email is empty',
+                        'Password is too short',
+                        'Name is too short'
+                    ]
+                }
+          }
+            #swagger.responses[409] = {
+                description : '이미 가입한 이메일',
+                schema : {
+                    message : 'Email is already use'
+                }
+          }
+         */
         const err = validationResult(req);
         if (validRequest(err)) {
             console.log(err);
@@ -71,7 +71,7 @@ router.post('/',
         const hashPw = await bcrypt.hash(password, salt);
 
         try {
-            const user = await Users.create({
+            const user = await User.create({
                 user_name: req.body.userName,
                 user_nickname: req.body.userNickname,
                 user_email: req.body.userEmail,
@@ -152,7 +152,7 @@ router.post('/login', [
         });
     }
 
-    const user = await getUser(userEmail);
+    const user = await getUserByEmail(userEmail);
     if (user.group_id === null) {
         return res.status(404).send({
             message: 'User not join group'
@@ -175,21 +175,68 @@ router.post('/login', [
     });
 })
 
+//유저 탈퇴
+router.delete('/:user_id', [
+    check('userPassword', 'Password is empty').trim().not().isEmpty()
+], async (req, res) => {
+
+    const err = validationResult(req);
+    if (validRequest(err)) {
+        return res.status(400).send({
+            message: err.array()[0].msg,
+        });
+    }
+
+    const userId = req.params.user_id;
+    const user = await findByUserId(userId);
+    if (user === null) {
+        return res.status(404).send({
+            message: 'User not found'
+        });
+    }
+
+    const password = req.body.userPassword;
+    bcrypt.compare(password, user.user_password, (err, same) => {
+        if (!same) {
+            return res.status(401).send({
+                message: 'Password is wrong'
+            });
+        }
+    });
+
+    try {
+        await User.destroy({
+            where: {user_id: userId}
+        });
+        return res.status(204).send({
+            message: 'User deleted'
+
+        });
+    } catch (e) {
+        console.error(e);
+    }
+})
+
+
 const validRequest = (error) => {
     return !error.isEmpty();
 }
 
 const findByEmail = async (email) => {
-    const user = await Users.findAll({
+    const user = await User.findAll({
         where: {user_email: email}
     });
     return user.length !== 0;
 }
 
-const getUser = async (email) => {
-    return await Users.findOne({
+const getUserByEmail = async (email) => {
+    return await User.findOne({
         where: {user_email: email}
     });
+}
+
+const findByUserId = async (id) => {
+    return await User.findByPk(id);
 }
 
 module.exports = router;
