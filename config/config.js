392bedd2ecd7d3da645725d11e5c812a4846e696
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-09 21:46:55 +0900

    #51 코드 라인 정리

--- config/config.js
+++ config/config.js
@@ -2,42 +2,42 @@ require('dotenv').config();
 const env = process.env;
 
 const development = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
-  host: env.DB_HOST,
-  dialect: "mysql",
-  timezone : '+09:00',
-  dialectOptions : {
-    dateStrings : true,
-    typeCast : true
-  }
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
+    host: env.DB_HOST,
+    dialect: "mysql",
+    timezone: '+09:00',
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true
+    }
 };
 
 const production = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
-  host: env.DB_HOST,
-  dialect: "mysql",
-  timezone : '+09:00',
-  dialectOptions : {
-    dateStrings : true,
-    typeCast : true
-  }
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
+    host: env.DB_HOST,
+    dialect: "mysql",
+    timezone: '+09:00',
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true
+    }
 };
 
 const test = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
-  host: env.DB_HOST,
-  dialect: "mysql",
-  timezone : '+09:00',
-  dialectOptions : {
-    dateStrings : true,
-    typeCast : true
-  }
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
+    host: env.DB_HOST,
+    dialect: "mysql",
+    timezone: '+09:00',
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true
+    }
 };
 
-module.exports = { development, production, test };
+module.exports = {development, production, test};
