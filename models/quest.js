a1248b723e9d8aad98e1a6dbe4fe01dcaa961ea0
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-12 20:11:56 +0900

    #60 timeStamp 추가 createdAt, updatedAt

--- models/quest.js
+++ models/quest.js
@@ -6,43 +6,43 @@ class Quest extends Sequelize.Model {
 
         return super.init(
             {
-                quest_id : {
-                    primaryKey : true,
-                    autoIncrement : true,
-                    type : Sequelize.INTEGER
+                quest_id: {
+                    primaryKey: true,
+                    autoIncrement: true,
+                    type: Sequelize.INTEGER
                 },
-                quest_title : {
-                    type : Sequelize.STRING(20),
-                    allowNull : false
-                },
-                quest_content : {
-                    type : Sequelize.STRING(100),
+                quest_title: {
+                    type: Sequelize.STRING(20),
                     allowNull: false
                 },
-                accept_user_id : {
-                    type : Sequelize.INTEGER,
-                    allowNull : false,
-                    defaultValue : -1
+                quest_content: {
+                    type: Sequelize.STRING(100),
+                    allowNull: false
                 },
-                complete_check : {
-                    type : Sequelize.BOOLEAN,
-                    allowNull : false
+                accept_user_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    defaultValue: -1
+                },
+                complete_check: {
+                    type: Sequelize.BOOLEAN,
+                    allowNull: false
                 }
             },
             {
                 sequelize,
-                timestamps:false,
-                tableName : 'quest_tb',
-                modelName : 'Quest',
-                charset : 'utf8',
-                collate : 'utf8_general_ci'
+                timestamps: true,
+                tableName: 'quest_tb',
+                modelName: 'Quest',
+                charset: 'utf8',
+                collate: 'utf8_general_ci'
             }
         );
     }
 
     static associate(db) {
-        db.Quest.belongsTo(db.Group, {foreignKey : 'group_id', targetKey : 'group_id'});
-        db.Quest.belongsTo(db.User, {foreignKey : 'request_user_id', targetKey : 'user_id'});
+        db.Quest.belongsTo(db.Group, {foreignKey: 'group_id', targetKey: 'group_id'});
+        db.Quest.belongsTo(db.User, {foreignKey: 'request_user_id', targetKey: 'user_id'});
     }
 }
 
