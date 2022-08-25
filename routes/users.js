898a40e255b0b0ad4fb83a68595212be9d255a54
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-25 20:41:13 +0900

    #42 회원 상세 조회 기능 구현

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const User = require('../models/user');
 const {validationResult, check} = require("express-validator");
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const moment = require("moment");
+const Chore = require("../models/chore");
+const {sequelize} = require("../models");
+const {Op} = require("sequelize");
+const Quest = require("../models/quest");
 
 //유저 생성
 router.post('/',
@@ -323,6 +328,95 @@ router.put('/:user_id/password', [
     });
 });
 
+//유저 상세 조회
+router.get('/details/:user_id', async (req, res) => {
+    const userId = req.params.user_id * 1;
+    const user = await findByUserId(userId);
+    if (user === null) {
+        return res.status(404).send({
+            message: '해당하는 유저가 없습니다.'
+        });
+    }
+
+    const date = new Date(req.query.date);
+    if (!validDateFormat(date)) {
+        return res.status(400).send({
+            message: '날짜 형식이 올바르지 않습니다.'
+        });
+    }
+
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+
+    const king = {};
+    let choreMap = new Map();
+    let choreKing = [];
+    try {
+        const list = await Chore.findAll({
+            attributes: [['chore_category', 'category'],
+                [sequelize.fn('count', '*'), 'count'], ['chore_user_id', 'userId']],
+            raw: true,
+            group: ['chore_category', 'chore_user_id'],
+            where: {
+                chore_check: 'SUCCESS',
+                [Op.and]: [
+                    sequelize.where(sequelize.fn('year', sequelize.col('chore_date')), '=', year),
+                    sequelize.where(sequelize.fn('month', sequelize.col('chore_date')), '=', month)
+                ]
+            },
+            order: [
+                [sequelize.fn('count', sequelize.col('*')), 'desc'],
+                [sequelize.literal('chore_category'), 'asc']
+            ]
+        });
+
+        for (let i of list) {
+            if (!choreMap.has(i.category) && i.userId === userId) {
+                choreMap.set(i.category, i);
+            }
+        }
+        if (choreMap.size !== 0) {
+            for (let i of choreMap.values()) {
+                choreKing = choreKing.concat(i);
+            }
+        }
+
+        king.choreKing = choreKing;
+
+        const questKing = await Quest.findOne({
+            attributes: [[sequelize.fn('count', '*'), 'count'], ['accept_user_id', 'userId']],
+            group: ['accept_user_id'],
+            raw : true,
+            where: {
+                complete_check: true,
+                [Op.and]: [
+                    sequelize.where(sequelize.fn('year', sequelize.col('createdAt')), '=', year),
+                    sequelize.where(sequelize.fn('month', sequelize.col('createdAt')), '=', month)
+                ]
+            }
+        });
+
+        if (questKing.userId === userId) {
+            king.questKing = questKing;
+        }
+        else {
+            king.questKing = [];
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    const resUser = {
+        userName: user.user_name,
+        userNickname: user.user_nickname,
+        userEmail: user.user_email,
+        king: king
+    };
+
+    return res.status(200).json(resUser);
+});
+
 
 const validRequest = (error) => {
     return !error.isEmpty();
@@ -343,6 +437,10 @@ const findByUserEmail = async (email) => {
 
 const findByUserId = async (id) => {
     return await User.findByPk(id);
+}
+
+const validDateFormat = (date) => {
+    return moment(date, 'YYYY-MM').isValid();
 }
 
 module.exports = router;
